75848e8708d29dd5dbb512e7d6900cd212c9a1cf
Ritik Saini(dev-rishiz) <ritikksainii26@gmail.com>
2025-06-09 20:20:23 +0530

    loding effect updated

--- src/component/Recipe.jsx
+++ src/component/Recipe.jsx
@@ -1,16 +1,3 @@
-// import Markdown from "react-markdown"
-
-
-// const Recipe = (props) => {
-//   return (
-//   <div  className="suggested-recipe-container">
-//     <Markdown>{props.recipe}</Markdown>
-//   </div>
-    
-//   )
-// }
-
-// export default Recipe
 
 import Markdown from "react-markdown"
 
